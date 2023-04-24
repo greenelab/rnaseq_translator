b06e6d350a6739af2d5d316082bb890ced4ddc28
ivichadriana <ivichadriana@gmail.com>
2023-04-24 10:46:37 -0600

    The following commit contains the updated and annotated files from the linear regression analysis of SN, Sc and bulk. This commit is ready for a pull request.

--- exploratory_experiments/translator_fcn.py
+++ exploratory_experiments/translator_fcn.py
@@ -28,7 +28,6 @@ import os, sys
 import pickle
 from pathlib import Path
 
-
 def linear_model_cell_gene(adata, obs_var, obs_cat_X, obs_cat_Y, num_test, num_train):
     #based on https://scikit-learn.org/stable/auto_examples/linear_model/plot_ols.html
     #this fcn withholds genes to train and test
@@ -80,7 +79,6 @@ def linear_model_cell_gene(adata, obs_var, obs_cat_X, obs_cat_Y, num_test, num_t
 
     return(regr, x_train, y_train, x_test, y_test, y_pred)
 
-
 def linear_model_cellnum(adata, obs_var, obs_cat_X, obs_cat_Y, pc_test):
     #based on https://scikit-learn.org/stable/auto_examples/linear_model/plot_ols.html
     #this fcn withholds cells to test and train
@@ -99,8 +97,8 @@ def linear_model_cellnum(adata, obs_var, obs_cat_X, obs_cat_Y, pc_test):
     idx1 = int(len(y)*pc_test)
     y_test= np.mean(y[0:idx1,:], axis=0) 
     y_test = y_test[:,np.newaxis]
-    idx2 = int(len(x)*pc_train)
-    y_train = np.mean(x[idx2:-1,:], axis=0)
+    idx2 = int(len(y)*pc_train)
+    y_train = np.mean(y[idx2:-1,:], axis=0)
     y_train = y_train[:,np.newaxis]
 
     # Create linear regression object
@@ -130,7 +128,6 @@ def linear_model_cellnum(adata, obs_var, obs_cat_X, obs_cat_Y, pc_test):
     plt.show()
 
     return(regr, x_train, y_train, x_test, y_test, y_pred)
-
 
 def linear_model_diffcell_gene(adata, obs_var, x, ct1, ct2, pc_test):
     #based on https://scikit-learn.org/stable/auto_examples/linear_model/plot_ols.html 
@@ -173,8 +170,8 @@ def linear_model_diffcell_gene(adata, obs_var, x, ct1, ct2, pc_test):
     idx1 = int(len(yte)*pc_test)
     y_test= yte[0:idx1]
     y_test = y_test[:,np.newaxis]
-    idx2 = int(len(xtr)*pc_train)
-    y_train = xtr[idx2:-1]
+    idx2 = int(len(ytr)*pc_train)
+    y_train = ytr[idx2:-1]
     y_train = y_train[:,np.newaxis]
 
     # Create linear regression object
@@ -239,8 +236,8 @@ def linear_model_diffcellnum(adata, obs_var, x, ct1, ct2, pc_test):
     idx1 = int(len(yte)*pc_test)
     y_test= np.mean(yte[0:idx1,:], axis=0) 
     y_test = y_test[:,np.newaxis]
-    idx2 = int(len(xtr)*pc_train)
-    y_train = np.mean(xtr[idx2:-1,:], axis=0)
+    idx2 = int(len(ytr)*pc_train)
+    y_train = np.mean(ytr[idx2:-1,:], axis=0)
     y_train = y_train[:,np.newaxis]
 
     # Create linear regression object
@@ -271,6 +268,86 @@ def linear_model_diffcellnum(adata, obs_var, x, ct1, ct2, pc_test):
 
     return(regr, x_train, y_train, x_test, y_test, y_pred)    
 
+def linear_model_bulked(adata1, adata2, pc_test, by_what, x,y):
+
+    #Split the data into training/testing sets
+    pc_train = 1 - pc_test
+    if by_what == "genes":
+        axx = 1
+        idx1 = int(adata1.X.shape[axx]*pc_test) 
+        if idx1 < 1:
+            idx1 = 1
+        x_test= adata1.X[:,0:idx1]
+        x_train= adata1.X[:,idx1:-1]
+        print(f"xtrain is {x_train.shape}")
+        idx2 = int(adata2.X.shape[axx]*pc_test)
+        if idx2 < 1:
+            idx2 = 1
+            by_what = "genes"
+        y_test= adata2.X[:,0:idx2]
+        y_train= adata2.X[:,idx2:-1]
+        print(f"ytrain is {y_train.shape}")
+    elif by_what == "cells":
+        axx = 0
+        idx1 = int(adata1.X.shape[axx]*pc_test) 
+        if idx1 < 1:
+            print("Can't separate by cells, doing by genes")
+            idx1 = 1    
+            by_what = "genes"
+        x_test= adata1.X[0:idx1,:]
+        x_train= adata1.X[idx1:-1,:]
+        print(f"xtrain is {x_train.shape}")
+        idx2 = int(adata2.X.shape[axx]*pc_test)
+        if idx2 < 1:
+            print("Can't separate by cells, doing by genes")
+            idx2 = 1
+            by_what = "genes"
+        y_test = adata2.X[0:idx2,:]
+        y_train= adata2.X[idx2:-1,:]
+        print(f"ytrain is {y_train.shape}")
+
+    #taking average across all cells of the same celltype, keeping all genes or cells
+    y_train= np.mean(y_train, axis=0) #SC
+    y_test = np.mean(y_test, axis=0) #SC
+    x_train = np.mean(x_train, axis=0) #SN
+    x_test = np.mean(x_test, axis=0) #SN
+
+    x_test = x_test[:,np.newaxis] #creating new axis so we can fit linear model
+    #repeating for the others:
+    x_train = x_train[:,np.newaxis]
+    y_test = y_test[:,np.newaxis]
+    y_train = y_train[:,np.newaxis]
+
+    # Create linear regression object
+    regr = linear_model.LinearRegression()
+    # Train the model using the training sets
+    regr.fit(x_train, y_train)
+
+    # Make predictions using the testing set
+    y_pred = regr.predict(x_test)
+
+    # The coefficients
+    print("Coefficients: \n", regr.coef_)
+    # The mean squared error
+    print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
+    # The coefficient of determination: 1 is perfect prediction
+    print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
+
+    # Plot outputs
+    plt.figure(figsize = [10,10])
+    plt.scatter(x_test, y_test, color="green")
+    plt.scatter(x_test, y_pred, color="black")
+    plt.plot(x_test, y_pred, color="blue", linewidth=3)
+    plt.title(f"LinearReg Predition of Bulked {y} using Bulked {x} by {by_what}")
+    plt.xlabel(f"{x}")
+    plt.ylabel(f"{y}")
+    plt.xticks()
+    plt.yticks()
+    plt.grid(())
+    plt.show()
+
+    return(regr, x_train, y_train, x_test, y_test, y_pred)
+
 def make_prop_table(adata, obs):
     num_cell_counter = Counter(adata.obs[obs])
     num_cells = list()
@@ -298,7 +375,6 @@ def make_prop_table(adata, obs):
     table = pd.DataFrame(table)
     print(tabulate(table,  headers='keys', tablefmt='fancy_grid', showindex = True))
     return table        
-
 
 #funtion from https://github.com/greenelab/sc_bulk_ood/blob/main/evaluation_experiments/pbmc/pbmc_experiment_perturbation.ipynb
 def mean_sqr_error(single1, single2):
